89278dfedf27120b221f995148671e7c735cfce9
Kevin Klopfenstein <kklopfenstein@gmail.com>
2026-05-25 10:01:17 -0400

    update target to remove

--- target/debug/deps/log_server.d
+++ target/debug/deps/log_server.d
@@ -1,5 +1,6 @@
-C:\Users\kklop\code\log-server\target\debug\deps\log_server.d: src\main.rs
+C:\Users\kklop\code\log-server\target\debug\deps\log_server.d: src\main.rs src\handlers.rs
 
-C:\Users\kklop\code\log-server\target\debug\deps\log_server.exe: src\main.rs
+C:\Users\kklop\code\log-server\target\debug\deps\log_server.exe: src\main.rs src\handlers.rs
 
 src\main.rs:
+src\handlers.rs:
